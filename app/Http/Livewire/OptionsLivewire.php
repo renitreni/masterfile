7f22694ee91ef7fa1e58dbdc4711a623410e77f0
Renier <17608701+fugazi-code@users.noreply.github.com>
2023-03-08 08:12:21 +0800

    update

--- app/Http/Livewire/OptionsLivewire.php
+++ app/Http/Livewire/OptionsLivewire.php
@@ -4,6 +4,8 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\ApplicantCategory;
+use App\Models\ApplicantType;
+use App\Models\Religion;
 use Illuminate\Support\Facades\Validator;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 
@@ -14,10 +16,20 @@ class OptionsLivewire extends Component
     public $applicantCategory;
 
     public $applicantCategoryText;
+    
+    public $applicantType;
+
+    public $applicantTypeText;
+
+    public $religion;
+
+    public $religionText;
 
     public function render()
     {
         $this->applicantCategory = ApplicantCategory::all();
+        $this->applicantType = ApplicantType::all();
+        $this->religion = Religion::all();
 
         return view('livewire.options-livewire');
     }
@@ -39,6 +51,49 @@ class OptionsLivewire extends Component
         ApplicantCategory::create([
             'name' => $this->applicantCategoryText
         ]);
+        $this->applicantCategoryText = '';
+        $this->alert('success', 'Added!');
+    }
+
+    public function destroyApplicantType($id)
+    {
+        $validator = Validator::make(['id' => $id], ['id' => 'required|exists:applications,applicant_type_id']);
+
+        if ($validator->fails()) {
+            ApplicantType::destroy($id);
+            $this->alert('success', 'Deleted!');
+         } else {
+            $this->alert('warning', 'Cannot Delete! Data is being used!');
+         }
+    }
+
+    public function addApplicantType()
+    {
+        ApplicantType::create([
+            'name' => $this->applicantTypeText
+        ]);
+        $this->applicantTypeText = '';
+        $this->alert('success', 'Added!');
+    }
+
+    public function destroyReligion($id)
+    {
+        $validator = Validator::make(['id' => $id], ['id' => 'required|exists:applications,religion_id']);
+
+        if ($validator->fails()) {
+            Religion::destroy($id);
+            $this->alert('success', 'Deleted!');
+         } else {
+            $this->alert('warning', 'Cannot Delete! Data is being used!');
+         }
+    }
+
+    public function addReligion()
+    {
+        Religion::create([
+            'name' => $this->religionText
+        ]);
+        $this->religionText = '';
         $this->alert('success', 'Added!');
     }
 }
--- resources/views/livewire/options-livewire.blade.php
+++ resources/views/livewire/options-livewire.blade.php
@@ -30,5 +30,59 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Applicant Type
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 d-flex flex-row mb-3">
+                            <input type="text" class="form-control" wire:model.debounce='applicantTypeText'>
+                            <button class="btn btn-primary" type="button" wire:click='addApplicantType'>Add</button>
+                        </div>
+                        <div class="col-12">
+                            <ul class="list-group">
+                                @foreach ($applicantType as $category)
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        <div>{{ $category->name }}</div>
+                                        <div>
+                                            <button type="button" class="btn btn-sm btn-danger" wire:click="destroyApplicantType('{{ $category->id }}')">Remove</button>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header">
+                    Religions
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 d-flex flex-row mb-3">
+                            <input type="text" class="form-control" wire:model.debounce='religionText'>
+                            <button class="btn btn-primary" type="button" wire:click='addReligion'>Add</button>
+                        </div>
+                        <div class="col-12">
+                            <ul class="list-group">
+                                @foreach ($religion as $category)
+                                    <li class="list-group-item d-flex justify-content-between">
+                                        <div>{{ $category->name }}</div>
+                                        <div>
+                                            <button type="button" class="btn btn-sm btn-danger" wire:click="destroyReligion('{{ $category->id }}')">Remove</button>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
